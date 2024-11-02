2453d2c5c8308f40aca69596640f178461a940f6
pblsh <pablodelah@uma.es>
2024-11-02 21:48:15 +0100

    Cambiado <a> por <Link>
    Cambiado por error en Vercel

--- code/front-end/tfg/src/app/components/navbar.tsx
+++ code/front-end/tfg/src/app/components/navbar.tsx
@@ -5,136 +5,137 @@ import Link from "next/link"
 
 import { cn } from "@/lib/utils"
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
 
 const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Modelo de lenguaje",
-    href: "*",
-    description:
-    "Usamos *"
-},
-  {
-    title: "OCR",
-    href: "*",
-    description:
-      "La trascripción de imágen a texto con *",
-  },
-  {
-    title: "Front-end",
-    href: "*",
-    description:
-      "Next.js como tecnología de presentación",
-  },
-  {
-    title: "Back-end",
-    href: "*",
-    description: "Fast-API como tecnología de *",
-  },
-  {
-    title: "Arquitectura",
-    href: "*",
-    description:
-      "Arquitectura en microservicios",
-  },
-  {
-    title: "Base de datos",
-    href: "*",
-    description:
-      "MongoDB como base de datos",
-  },
+    {
+        title: "Modelo de lenguaje",
+        href: "*",
+        description:
+            "Usamos *"
+    },
+    {
+        title: "OCR",
+        href: "*",
+        description:
+            "La trascripción de imágen a texto con *",
+    },
+    {
+        title: "Front-end",
+        href: "*",
+        description:
+            "Next.js como tecnología de presentación",
+    },
+    {
+        title: "Back-end",
+        href: "*",
+        description: "Fast-API como tecnología de *",
+    },
+    {
+        title: "Arquitectura",
+        href: "*",
+        description:
+            "Arquitectura en microservicios",
+    },
+    {
+        title: "Base de datos",
+        href: "*",
+        description:
+            "MongoDB como base de datos",
+    },
 ]
 
 export default function NavBar() {
-  return (
-    <NavigationMenu className="border=1">
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Inicio</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      Motivación
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Aplicación para la detección y denuncia de delitos de odio, enfocada para ser un referente en la lucha antirracista
-                      LaLiga.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/*" title="Introducción">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/*" title="Uso">
-                How to install dependencies and structure your app.
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>¿Cómo funciona?</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-  )
+    return (
+        <NavigationMenu className="border=1">
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>Inicio</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                            <li className="row-span-3">
+                                <NavigationMenuLink asChild>
+                                    <Link
+                                        href="/"
+                                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                                    >
+                                        <div className="mb-2 mt-4 text-lg font-medium">
+                                            Motivación
+                                        </div>
+                                        <p className="text-sm leading-tight text-muted-foreground">
+                                            Aplicación para la detección y denuncia de delitos de odio, enfocada para ser un referente en la lucha antirracista
+                                            LaLiga.
+                                        </p>
+                                    </Link>
+                                </NavigationMenuLink>
+
+                            </li>
+                            <ListItem href="/*" title="Introducción">
+                                Re-usable components built using Radix UI and Tailwind CSS.
+                            </ListItem>
+                            <ListItem href="/*" title="Uso">
+                                How to install dependencies and structure your app.
+                            </ListItem>
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>¿Cómo funciona?</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                            {components.map((component) => (
+                                <ListItem
+                                    key={component.title}
+                                    title={component.title}
+                                    href={component.href}
+                                >
+                                    {component.description}
+                                </ListItem>
+                            ))}
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/docs" legacyBehavior passHref>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Documentation
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
 }
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+    React.ElementRef<"a">,
+    React.ComponentPropsWithoutRef<"a">
 >(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  )
+    return (
+        <li>
+            <NavigationMenuLink asChild>
+                <a
+                    ref={ref}
+                    className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        className
+                    )}
+                    {...props}
+                >
+                    <div className="text-sm font-medium leading-none">{title}</div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        {children}
+                    </p>
+                </a>
+            </NavigationMenuLink>
+        </li>
+    )
 })
 ListItem.displayName = "ListItem"
