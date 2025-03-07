cda4ba00a53dccfef5c3039e7de8aa0af00259b8
pSenciales <124668501+pSenciales@users.noreply.github.com>
2025-03-07 14:22:01 +0100

    fix: "handler" is not a valid Route export field

--- code/front-end/tfg/src/app/api/auth/[...nextauth]/route.ts
+++ code/front-end/tfg/src/app/api/auth/[...nextauth]/route.ts
@@ -65,6 +65,5 @@ export const authOptions: NextAuthOptions = {
   },
 };
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
-export default handler;
+export const GET = NextAuth(authOptions);
+export const POST = NextAuth(authOptions);
